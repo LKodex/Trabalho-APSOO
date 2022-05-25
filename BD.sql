e06f307c6d07df566bffd4398beb1db740f57602
LKodex <lucasgon192@gmail.com>
2022-05-25 00:36:33 -0300

    Correção de FKs
    Corrigido chaves, estrangeiras e primárias

--- BD.sql
+++ BD.sql
@@ -1,29 +1,29 @@
 CREATE TABLE IF NOT EXISTS Pessoa(
 	cpf char(11) PRIMARY KEY,
 	nome varchar(100),
-	FOREIGN KEY(cpf) REFERENCES Cliente(cpf),
-	FOREIGN KEY(cpf) REFERENCES Funcionario(cpf)
 );
 CREATE TABLE IF NOT EXISTS Cliente(
+	cpf char(11) PRIMARY KEY,
 	celular char(11),
-	cpf char(11) PRIMARY KEY
+	FOREIGN KEY(cpf) REFERENCES Pessoa(cpf)
 );
 CREATE TABLE IF NOT EXISTS Funcionario(
-	senha char(256),
-	cpf char(11) PRIMARY KEY
+	cpf char(11) PRIMARY KEY,
+	senha char(256) NOT NULL,
+	FOREIGN KEY(cpf) REFERENCES Pessoa(cpf)
 );
 CREATE TABLE IF NOT EXISTS Locacao(
 	id int PRIMARY KEY,
-	cpf char(11) NOT NULL,
+	cpfCliente char(11) NOT NULL,
+	cpfFuncionario char(11) NOT NULL,
 	inicioPrevisto date,
 	fimPrevisto date,
 	dataReservada date,
 	endereco varchar(255),
-	FOREIGN KEY(cpf) REFERENCES Funcionario(cpf),
-	FOREIGN KEY(cpf) REFERENCES Cliente(cpf),
-	FOREIGN KEY(id) REFERENCES ArtigoLocado(id)
+	FOREIGN KEY(cpfFuncionario) REFERENCES Funcionario(cpf),
+	FOREIGN KEY(cpfCliente) REFERENCES Cliente(cpf)
 );
-CREATE TABLE IF NOT EXISTS Artigos(
+CREATE TABLE IF NOT EXISTS Artigo(
 	codigo int PRIMARY KEY,
 	nomeArtigo varchar(100),
 	valorDiario float(53),
@@ -35,6 +35,6 @@ CREATE TABLE IF NOT EXISTS ArtigoLocado(
 	quantidade int,
 	valorCotado float(53),
 	valorTotal float(53),
-	FOREIGN KEY (codigo) REFERENCES Artigos (codigo),
+	FOREIGN KEY(codigo) REFERENCES Artigo(codigo),
 	FOREIGN KEY(id) REFERENCES Locacao(id)
 );
